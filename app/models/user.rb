7bd01967f9554be1c82572644d71d8bfa1d94b9b
chris-wong-1 <wong.christopher.s@gmail.com>
2016-08-16 13:14:30 -0700

    Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  validates_presence_of :username, :email, :password
+  validates :username, uniqueness: { case_sensitive: false }
+  validates :email, uniqueness: { case_sensitive: false }
 end
