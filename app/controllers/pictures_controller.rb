b7c7cb2a73f2ce5dfa6a975cda0c76c05dad3933
chris-wong-1 <wong.christopher.s@gmail.com>
2016-09-01 17:45:17 -0700

    Update pictures controller and views

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,8 +1,24 @@
 class PicturesController < ApplicationController
+  def index
+    @pictures = Picture.all
+  end
+
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:picture].inspect
+    @picture = Picture.new(picture_params)
+
+    @picture.save
+    redirect_to @picture
   end
+
+  private
+    def picture_params
+      params.require(:picture).permit(:title)
+    end
 end
